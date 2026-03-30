51f01f61cb2af3fc94f308d31afe2239460d06e5
ketterrm <ketterrm@gmail.com>
2026-03-30 20:02:16 +0000

    Update launch.json for Django run/debug with CODESPACE_NAME support

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('octofit_tracker.urls_api')),
+    path('api/', include('octofit_tracker.urls_api')),
 ]
